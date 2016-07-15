9468a78bebd7c4525cdbf219238fa19ecb24004d
Chris Su <crystalsu2008@gmail.com>
2016-07-15 11:18:32 +0800

    Update and rename mayaMiscTools.py to maya_misc_tools.py

--- maya_misc_tools.py
+++ maya_misc_tools.py
@@ -1,27 +1,31 @@
-import bpf #Tools-1, Batch Processing Files
-#reload(bpf)
-from pymel.core import *
+# This module is a collection that include all tool's module files and organize the UI.
 
-class mayaMiscTools():
-    
-    def initUI(self): 
+from pymel.core import *
+from unique_name_manager import *
+from batch_processing_files import *
+
+def openUI():
+    MayaMiscTools().openUI()
+
+class MayaMiscTools(object):
+    'The base class'
+
+    def initUI(self):
         self.win=window('mayaMiscTools',t='Maya Misc Tools',w=400)
         with formLayout() as self.form:
             with tabLayout(innerMarginWidth=5, innerMarginHeight=5) as self.tabs:
                 #embed tab1
-                tab1=bpf.batchProcessingFiles()
+                tab1=UniqueNameManager()
                 tab1.initUI(self.tabs)
+                #embed tab2
+                tab2=BatchProcessingFiles()
+                tab2.initUI(self.tabs)
 
         formLayout( self.form, edit=True, attachForm=((self.tabs, 'top', 0), (self.tabs, 'left', 0), (self.tabs, 'bottom', 0), (self.tabs, 'right', 0)) )
-        tabLayout( self.tabs, edit=True, tabLabel=(tab1.embed, tab1.lable) )
+        tabLayout( self.tabs, edit=True, tabLabel=((tab1.embed, tab1.lable), (tab2.embed, tab2.lable)) )
 
     def openUI(self):
         if window('mayaMiscTools', q=True, ex=True):
             deleteUI('mayaMiscTools', wnd=True)
         self.initUI()
         showWindow(self.win)
-
-mmt=MayaMiscTools()
-mmt.openUI()
-
-
